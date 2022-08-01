3f79b0af86d3a292dc8ad4dfa694a8275aa977c3
“pascal”-maker <pascal-musa@hotmail.com>
2022-08-01 13:06:14 +0200

    avatarlogo

---
+++ avatarlogo.py
@@ -0,0 +1,19 @@
+from turtle import *
+speed(0)
+bgcolor('black')
+color('orange')
+hideturtle()
+n=1
+p=True
+while True:
+    circle(n)
+    if p:
+        n=n-1
+else:
+         n= n+1
+         if n==0 or n==60:
+                p=not p
+                left(1)
+                forward(3)
+                #ccloding.com
+
